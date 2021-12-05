a740a71f02219fabf4dc6b0aaf55af003b58030e
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-12-05 11:50:17 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
